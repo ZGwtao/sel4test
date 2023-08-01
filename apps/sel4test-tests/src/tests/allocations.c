23ac429e480ad6edcde2d57c3105c4244de30d9e
zhuguangtao@iie.ac.cn <zhuguangtao@iie.ac.cn>
2023-08-01 18:35:07 +0800

    [testcase/allocations.c] create new deallocation testcases & modify name convention
    1. ALLOC000X -> AXMR_ALLOC000X Modify testcase name convention
    2. create 'AXMR_FREE0001', 'AXMR_FREE0002'
    
    Signed-off-by: zhuguangtao@iie.ac.cn <zhuguangtao@iie.ac.cn>

--- apps/sel4test-tests/src/tests/allocations.c
+++ apps/sel4test-tests/src/tests/allocations.c
@@ -154,14 +154,14 @@ static int test_frame_allocation_single(env_t env)
     seL4_BenchmarkFinalizeLog();
     seL4_BenchmarkGetThreadUtilisation(simple_get_tcb(&env->simple));
     printf("\n---------- Utilisation ----------\n");
-    printf("\nAverage cycles per alloc: %ld\n", ipcbuffer[BENCHMARK_TCB_UTILISATION] / frame_cnt);
+    printf("\nAverage cycles per alloc: %ld, cnt: %ld\n", ipcbuffer[BENCHMARK_TCB_UTILISATION] / cnt, cnt);
     printf("\nCPU cycles spent: %ld\n\n", ipcbuffer[BENCHMARK_TCB_UTILISATION]);
-    printf("\n=========== Benchmark ===========\n");
+    printf("\n=========== Benchmark ===========\n\n");
 #endif
     return sel4test_get_result();
 }
 
-DEFINE_TEST(ALLOC0001, "Testcase for testing frame allocations for CapBuddy", test_frame_allocation_single, true)
+DEFINE_TEST(AXMR_ALLOC0001, "Testcase for testing frame allocations for CapBuddy", test_frame_allocation_single, true)
 
 static int test_frame_allocation_contiguous(env_t env)
 {
@@ -171,7 +171,7 @@ static int test_frame_allocation_contiguous(env_t env)
      *  @param: batch -- frame number of memory request (based on 'bib')
      *  @param: frame_cnt -- available frames (overall) to meet all requests
      */
-    int bib = 6;
+    int bib = 4;
     int batch = BIT(bib);
 #if ENABLE_CAPBUDDY_EXTENSION
     size_t frame_cnt = test_collect_untypeds(&env->vka, 22);
@@ -183,7 +183,7 @@ static int test_frame_allocation_contiguous(env_t env)
     assert(frame_cnt % batch == 0);
 
     int err;
-    int cnt = 0;
+    size_t cnt = 0;
 
     /* below is a serie of temporary varibale */
     /* no need to free -> no need to do the bookkeeping */
@@ -233,12 +233,267 @@ static int test_frame_allocation_contiguous(env_t env)
     seL4_BenchmarkFinalizeLog();
     seL4_BenchmarkGetThreadUtilisation(simple_get_tcb(&env->simple));
     printf("\n---------- Utilisation ----------\n");
-    printf("\nAverage cycles per alloc: %ld\n", ipcbuffer[BENCHMARK_TCB_UTILISATION] / cnt);
+    printf("\nAverage cycles per alloc: %ld, cnt: %ld\n", ipcbuffer[BENCHMARK_TCB_UTILISATION] / cnt, cnt);
     printf("\nCPU cycles spent: %ld\n\n", ipcbuffer[BENCHMARK_TCB_UTILISATION]);
-    printf("\n=========== Benchmark ===========\n");
+    printf("\n=========== Benchmark ===========\n\n");
 #endif
     return sel4test_get_result();
 }
 
-DEFINE_TEST(ALLOC0002, "Testcase for testing frame allocations (contiguous) for CapBuddy",
+DEFINE_TEST(AXMR_ALLOC0002, "Testcase for testing frame allocations (contiguous) for CapBuddy",
             test_frame_allocation_contiguous, true)
+
+static int test_frame_deallocation_single(env_t env)
+{
+#if ENABLE_CAPBUDDY_EXTENSION
+    size_t frame_cnt = test_collect_untypeds(&env->vka, 22);
+#else
+    size_t frame_cnt = test_collect_untypeds(&env->vka, 12);
+#endif
+    int err;
+    size_t cnt = 0;
+    size_t alloc_cnt;   /* due to metadata restriction, not all frames (frame_cnt) are available */
+    /***
+     * We should collect the freeing materials here and they are
+     * the pre-allocated frames. So one main difference between
+     * alloc and dealloc testcase is we should bookkeeping their
+     * metadata to free them later on.
+     */
+    untyped_object_t *head = (untyped_object_t *)malloc(sizeof(untyped_object_t));
+    if (!head) {
+        assert(0);
+    }
+    head = (untyped_object_t *)memset(head, 0, sizeof(untyped_object_t));
+    untyped_object_t *curr = head;
+    /***
+     * Start our allocation here
+     */
+    while (cnt < frame_cnt) {
+        err = vka_alloc_frame(&env->vka, seL4_PageBits, &curr->target);
+        if (err != seL4_NoError) {
+            /* ZF_LOGE("Failed to allocate frame no.[%d]", cnt); */
+            break;
+        }
+        curr->next = (untyped_object_t *)malloc(sizeof(untyped_object_t));
+        if (!curr->next) {
+            break;
+        }
+        curr = curr->next;
+        cnt += 1;
+    }
+    alloc_cnt = cnt;
+    /***
+     * Now it's time to free them one by one.
+     */
+    cnt = 0;
+#ifdef CONFIG_KERNEL_BENCHMARK
+    printf("\n*********** Benchmark ***********\n\n");
+    uint64_t *ipcbuffer = (uint64_t *)&(seL4_GetIPCBuffer()->msg[0]);
+    seL4_BenchmarkResetThreadUtilisation(simple_get_tcb(&env->simple));
+    seL4_BenchmarkResetLog();
+#endif
+    while (cnt < alloc_cnt) {
+        if (head->target.cptr) {
+            vka_free_object(&env->vka, &head->target);
+        } else {
+            break;
+        }
+        curr = head;
+        head = head->next;
+        /* free(curr); */ // avoid it to not interfere with performance
+        cnt += 1;
+    }
+#ifdef CONFIG_KERNEL_BENCHMARK
+    seL4_BenchmarkFinalizeLog();
+    seL4_BenchmarkGetThreadUtilisation(simple_get_tcb(&env->simple));
+    printf("\n---------- Utilisation ----------\n");
+    printf("\nAverage cycles per dealloc: %ld, cnt: %ld\n", ipcbuffer[BENCHMARK_TCB_UTILISATION] / cnt, cnt);
+    printf("\nCPU cycles spent: %ld\n\n", ipcbuffer[BENCHMARK_TCB_UTILISATION]);
+    printf("\n=========== Benchmark ===========\n\n");
+#endif
+    return sel4test_get_result();
+}
+
+DEFINE_TEST(AXMR_FREE0001, "Testcase for testing frame deallocations for CapBuddy",
+            test_frame_deallocation_single, true)
+
+typedef struct frame_object {
+    seL4_CPtr cptr;
+    struct frame_object *next;
+} frame_object_t;
+
+typedef struct untyped_object_frames {
+    vka_object_t target;
+    struct untyped_object_frames *next;
+    frame_object_t *frame_list;
+} untyped_object_frames_t;
+
+static int test_frame_deallocation_contiguous(env_t env)
+{
+    /***
+     * NOTICE:
+     *  @param: bib -- memory region size of memory request (can be modified)
+     *  @param: batch -- frame number of memory request (based on 'bib')
+     *  @param: frame_cnt -- available frames (overall) to meet all requests
+     */
+    int bib = 4;
+    int batch = BIT(bib);
+#if ENABLE_CAPBUDDY_EXTENSION
+    size_t frame_cnt = test_collect_untypeds(&env->vka, 22);
+#else
+    size_t frame_cnt = test_collect_untypeds(&env->vka, seL4_PageBits + bib);
+#endif
+    //!frame_cnt = frame_cnt % batch ? frame_cnt : frame_cnt - (frame_cnt % batch);
+    /* frame_cnt should be 2^n * batch */
+    assert(frame_cnt % batch == 0);
+
+    int err;
+    size_t cnt = 0;
+    size_t alloc_cnt;
+
+    /* below is a serie of temporary varibale */
+#if ENABLE_CAPBUDDY_EXTENSION
+    int fn; /* frame (number) metadata for the requested memory region */
+#endif
+    cspacepath_t path; /* frame (cspacepath) metadata for the requested memory region */
+
+    untyped_object_frames_t *head = (untyped_object_frames_t *)malloc(sizeof(untyped_object_frames_t));
+    if (!head) {
+        assert(0);
+    }
+    head = (untyped_object_frames_t *)memset(head, 0, sizeof(untyped_object_frames_t));
+    untyped_object_frames_t *curr = head;
+
+    frame_object_t *fx;
+    frame_object_t *ft;
+
+    while (cnt < frame_cnt) {
+#if ENABLE_CAPBUDDY_EXTENSION
+        err = vka_alloc_frame_contiguous(&env->vka, bib + seL4_PageBits, &fn, &curr->target);
+        if (err != seL4_NoError) {
+            ZF_LOGE("Failed to allocate frame no.[%d]", cnt);
+            break;
+        }
+        curr->next = (untyped_object_frames_t *)malloc(sizeof(untyped_object_frames_t));
+        if (!curr->next) {
+            assert(0);
+        }
+        curr = curr->next;
+        curr = (untyped_object_frames_t *)memset(curr, 0, sizeof(untyped_object_frames_t));
+#else
+    /***
+     * Allocate one untyped object from allocator through vka with size: bib+12
+     * We need to slice it into BIT(bib) frames through untyped_retype
+     */
+        err = vka_alloc_untyped(&env->vka, bib + seL4_PageBits, &curr->target);
+        if (err != seL4_NoError) {
+            ZF_LOGE("Failed to allocate frame no.[%d]", cnt);
+            break;
+        }
+        /***
+         * Create metadata (allocated on heap) for the frames dynamically
+         */
+        fx = (frame_object_t *)malloc(sizeof(frame_object_t));
+        if (!fx) {
+            ZF_LOGE("Failed to allocate frame metadata");
+            assert(0);
+        }
+        fx = (frame_object_t *)memset(fx, 0, sizeof(frame_object_t));
+        ft = fx;
+        /***
+         * Now start to create pages for the original untyped object
+         */
+        for (int i = 0; i < batch; ++i) {
+            err = vka_cspace_alloc(&env->vka, &ft->cptr);
+            if (err != seL4_NoError) {
+                ZF_LOGE("Not enough cspace slot for frame objects\n");
+                assert(0);
+            }
+            vka_cspace_make_path(&env->vka, ft->cptr, &path);
+            err = seL4_Untyped_Retype(curr->target.cptr, seL4_ARCH_4KPage, seL4_PageBits,
+                                      path.root, path.dest, path.destDepth, path.offset, 1);
+            if (err != seL4_NoError) {
+                ZF_LOGE("Failed to retype new frames from the givin untyped");
+                assert(0);
+            }
+            ft->next = (frame_object_t *)malloc(sizeof(frame_object_t));
+            if (!ft->next) {
+                assert(0);
+            }
+            ft = ft->next;
+            ft = (frame_object_t *)memset(ft, 0, sizeof(frame_object_t));
+        }
+
+        /***
+         * Store the created frame list in untyped_object_frames list, which means
+         * these newly created frames belongs to the original object (curr)
+         */
+        curr->frame_list = fx;
+        curr->next = (untyped_object_frames_t *)malloc(sizeof(untyped_object_frames_t));
+        if (!curr->next) {
+            assert(0);
+        }
+        curr = curr->next;
+        curr = (untyped_object_frames_t *)memset(curr, 0, sizeof(untyped_object_frames_t));
+#endif
+        cnt += batch;
+    }
+    alloc_cnt = cnt;
+    
+    cnt = 0;
+#ifdef CONFIG_KERNEL_BENCHMARK
+    printf("\n*********** Benchmark ***********\n\n");
+    uint64_t *ipcbuffer = (uint64_t *)&(seL4_GetIPCBuffer()->msg[0]);
+    seL4_BenchmarkResetThreadUtilisation(simple_get_tcb(&env->simple));
+    seL4_BenchmarkResetLog();
+#endif
+
+    while (cnt < alloc_cnt) {
+#if ENABLE_CAPBUDDY_EXTENSION
+        if (head->target.cptr) {
+            vka_free_object(&env->vka, &head->target);
+        }
+#else
+        if (head->target.cptr) {
+            vka_cspace_make_path(&env->vka, head->target.cptr, &path);
+            err = seL4_CNode_Revoke(path.root, path.capPtr, path.capDepth);
+            if (err != seL4_NoError) {
+                ZF_LOGE("Failed to Revoke original untyped cptr");
+                assert(0);
+            }
+            fx = head->frame_list;
+            ft = fx;
+            for (int i = 0; i < batch; ++i) {
+                if (ft->cptr) {
+                    vka_cspace_free(&env->vka, ft->cptr);
+                } else {
+                    ZF_LOGE("Unable to free allocated frame capabilites");
+                    assert(0);
+                }
+                fx = fx->next;
+                free(ft); //! speed up performance
+                ft = fx;
+            }
+            head->frame_list = NULL;
+            vka_free_object(&env->vka, &head->target);
+        }
+#endif
+        curr = head;
+        head = head->next;
+        free(curr); //! speed up performance
+        cnt += batch;
+    }
+
+#ifdef CONFIG_KERNEL_BENCHMARK
+    seL4_BenchmarkFinalizeLog();
+    seL4_BenchmarkGetThreadUtilisation(simple_get_tcb(&env->simple));
+    printf("\n---------- Utilisation ----------\n");
+    printf("\nAverage cycles per dealloc: %ld, cnt: %ld\n", ipcbuffer[BENCHMARK_TCB_UTILISATION] / cnt, cnt);
+    printf("\nCPU cycles spent: %ld\n\n", ipcbuffer[BENCHMARK_TCB_UTILISATION]);
+    printf("\n=========== Benchmark ===========\n\n");
+#endif
+    return sel4test_get_result();    
+}
+
+DEFINE_TEST(AXMR_FREE0002, "Testcase for testing frame deallocations (contiguous) for CapBuddy",
+            test_frame_deallocation_contiguous, true)
