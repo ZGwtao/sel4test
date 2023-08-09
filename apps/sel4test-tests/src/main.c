b3a6dd966d35997e9212d3ce3a51c57444ff1fbb
guangtaozhu <guangtaozhu@outlook.com>
2023-08-09 22:08:09 +0800

    test-driver: clean up 'implicit declaration' error due to the intro of CapBuddy configuration
    Signed-off-by: guangtaozhu <guangtaozhu@outlook.com>

--- apps/sel4test-tests/src/main.c
+++ apps/sel4test-tests/src/main.c
@@ -110,17 +110,17 @@ static void init_allocator(env_t env, test_init_data_t *init_data)
          * which we don't. */
         size_bits = init_data->untyped_size_bits_list[size_bits_index];
         paddr = init_data->untyped_paddr_list[size_bits_index];
-        if (config_set(CONFIG_LIB_ALLOCMAN_ALLOW_POOL_OPERATIONS)) {
-            // 4M ~ 1G size untypeds are normally be used as memory
-            if (size_bits > 22 && size_bits < 28) {
-                // If CapBuddy is enabled, then we try to perform some spliting operations here.
-                for (int j = 0; j < BIT(size_bits - 22); ++j) {
-                    error = allocman_utspace_try_create_virtual_bitmap_tree(allocator, &path, 22 - seL4_PageBits, paddr + j * (1024 * 4096));
-                    ZF_LOGF_IF(error, "Could not create virtual-bitmap-tree from untyped");
-                }
-                continue;
+#ifdef CONFIG_LIB_ALLOCMAN_ALLOW_POOL_OPERATIONS
+        // 4M ~ 1G size untypeds are normally be used as memory
+        if (size_bits > 22 && size_bits < 28) {
+            // If CapBuddy is enabled, then we try to perform some spliting operations here.
+            for (int j = 0; j < BIT(size_bits - 22); ++j) {
+                error = allocman_utspace_try_create_virtual_bitmap_tree(allocator, &path, 22 - seL4_PageBits, paddr + j * (1024 * 4096));
+                ZF_LOGF_IF(error, "Could not create virtual-bitmap-tree from untyped");
             }
+            continue;
         }
+#endif
         error = allocman_utspace_add_uts(allocator, 1, &path, &size_bits, NULL,
                                          ALLOCMAN_UT_KERNEL);
         if (error) {
